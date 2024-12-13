bae21d128d7aee901dac3ab286bc7bfbaa48be8c
Xavier Ong <xavier.oyj1999@gmail.com>
2024-12-13 17:51:34 +0800

    Update site configuration in astro.config.mjs to set the site URL for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'paradisesolution.github.io',
   integrations: [
     react(),
     tailwind({
